13085fba6944f9989feb0c0a8b5cf20ac01328f8
omar auf <omar.auf.110@gmail.com>
2025-09-18 23:33:10 +0300

    refactor: remove facebook references from platform type schemas

--- packages/contracts/src/common/platform-type.ts
+++ packages/contracts/src/common/platform-type.ts
@@ -1,14 +1,14 @@
 import z from "zod";
 
-export const PlatformTypeArray = ["instagram", "twitter", "facebook"] as const;
+export const PlatformTypeArray = ["instagram", "twitter"] as const;
 export const PlatformTypeSchema = z.enum(PlatformTypeArray);
 export type PlatformType = z.infer<typeof PlatformTypeSchema>;
 export const PlatformTypeEnum = PlatformTypeSchema.enum;
 
-export const UserPlatformTypeSchema = z.enum(["instagram-user", "twitter-user", "facebook-user"]);
+export const UserPlatformTypeSchema = z.enum(["instagram-user", "twitter-user"]);
 export type UserPlatformType = z.infer<typeof UserPlatformTypeSchema>;
 export const UserPlatformTypeEnum = UserPlatformTypeSchema.enum;
 
-export const PostPlatformTypeSchema = z.enum(["instagram-post", "twitter-post", "facebook-post"]);
+export const PostPlatformTypeSchema = z.enum(["instagram-post", "twitter-post"]);
 export type PostPlatformType = z.infer<typeof PostPlatformTypeSchema>;
 export const PostPlatformTypeEnum = PostPlatformTypeSchema.enum;
